b6bac542f6738f5936623e44fa4495f401933529
Navarro Presas Moisés Alejandro <moisalejandro@gmail.com>
2019-12-09 18:11:41 -0600

    [MIG] 11.0 -> 12.0

--- __manifest__.py
+++ __manifest__.py
@@ -1,39 +1,14 @@
 # -*- coding: utf-8 -*-
-###############################################################################
-#
-#    Odoo, Open Source Management Solution
-#    Copyright (C) 2017 Humanytek (<www.humanytek.com>).
-#    Rubén Bravo <rubenred18@gmail.com>
-#
-#    This program is free software: you can redistribute it and/or modify
-#    it under the terms of the GNU Affero General Public License as
-#    published by the Free Software Foundation, either version 3 of the
-#    License, or (at your option) any later version.
-#
-#    This program is distributed in the hope that it will be useful,
-#    but WITHOUT ANY WARRANTY; without even the implied warranty of
-#    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
-#    GNU Affero General Public License for more details.
-#
-#    You should have received a copy of the GNU Affero General Public License
-#    along with this program.  If not, see <http://www.gnu.org/licenses/>.
-#
-###############################################################################
-
 {
     'name': "Kardex",
-    'summary': """
-    """,
-    'description': """
-    """,
     'author': "Humanytek",
-    'website': "http://www.humanytek.com",
+    'website': "https://github.com/humanytek-team/stock_kardex",
     'category': 'Stock',
-    'version': '1.0.0',
-    'depends': ['stock'],
+    'version': '12.0.1.0.0',
+    'depends': [
+        'stock',
+    ],
     'data': [
         'view/stock_kardex_view.xml',
-    ],
-    'demo': [
     ],
 }
